1fe9ad3dd4748d9d6dcf1481d8b979f30632ff4e
phrbmr <phrbmr@hotmail.com>
2024-05-11 19:54:00 -0300

    Creat HnS
    Some hear and no soul

---
+++ hearNsoul.rb
@@ -0,0 +1,56 @@
+
+
+
+in_thread do
+  loop do
+    play 60
+    sleep 0.25
+    play 60
+    sleep 0.25
+    
+    play 64
+    play 67
+    sleep 0.25
+    play 64
+    play 67
+    sleep 0.25
+    
+    play 57
+    sleep 0.25
+    play 57
+    sleep 0.25
+    
+    play 60
+    play 64
+    sleep 0.25
+    play 60
+    play 64
+    sleep 0.25
+    
+    play 53
+    sleep 0.25
+    play 53
+    sleep 0.25
+    
+    play 57
+    play 60
+    sleep 0.25
+    play 57
+    play 60
+    sleep 0.25
+    
+    play 55
+    sleep 0.25
+    play 55
+    sleep 0.25
+    
+    play 59
+    play 62
+    sleep 0.25
+    play 59
+    play 62
+    sleep 0.25
+  end
+end
+
+
